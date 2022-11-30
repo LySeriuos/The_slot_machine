808c051d91b2001634c21594b18f5bdc7137b5d3
LySeriuos <drungilas.p@gmail.com>
2022-11-30 21:47:58 +0100

    Add project files.

---
+++ The_slot_machine/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace The_slot_machine // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Plan for the slot machine code:
+            // 1. Instructions.
+
+
+
+
+
+
+            // 2. Choose option for bidding (player choose amount to play $). 
+            // 2.1 Create if statements to see if he wants to play combination of (vertical lines, horizontal lines, only center line, two horizontal lines...)
+
+
+
+
+
+            // 3. Create multi- random numbers.
+
+
+
+
+
+
+
+
+
+            // 4. Create 2D arrays for random numbers.
+
+
+
+
+
+
+
+
+
+
+
+            // 5. Create Random Numbers machine (slot machine).
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // 6. Create if statements for every combination.
+
+
+
+
+
+
+
+
+
+
+
+
+            // 7. Check if player win or lose.
+
+
+
+
+
+
+
+
+
+
+            // 8. Show how much money has player always, add winning, take out if lose.
+
+        }
+    }
+}
