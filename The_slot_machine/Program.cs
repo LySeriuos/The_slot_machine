fd74a0e281f67f83bece38dd57c85a0cbf79463a
LySeriuos <drungilas.p@gmail.com>
2023-01-08 20:12:06 +0100

    add function to check if central line has the same numbers

--- The_slot_machine/Program.cs
+++ The_slot_machine/Program.cs
@@ -1,13 +1,9 @@
-﻿using System;
-
-namespace The_slot_machine // Note: actual namespace depends on the project name.
+﻿namespace The_slot_machine // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             // Plan for the slot machine code:
             // 1. Instructions.
 
@@ -19,22 +15,20 @@ namespace The_slot_machine // Note: actual namespace depends on the project name
             Console.WriteLine("4. More lines yyou choosee more chance to win!");
             Console.WriteLine("5. Options to choose for the lines:  \n\t1. Center Line.\n\t2. All horizontal lines.\n\t3. All vertical lines.\n\t4. Diagonals.");
 
-
-
             // 2. Choose option for bidding (player choose amount to play $). 
             
+            // add if statement to check if input is empty or null
             Console.WriteLine("Add your amount of game money in USD $");
             int playersGameMoney = int.Parse(Console.ReadLine()); // Converting input to int directly, because later it will be used only as int
             Console.WriteLine("Choose how many lines you will play by typing in number of the menu");
             Console.WriteLine("\n\t1 - Center Line. One turn 1$\n\t2 - All horizontal lines. One turn 3$\n\t3 - All vertical lines. One turn 3$\n\t4 - Diagonals. One turn 2$");
+
             int playersChoseOptionToPLay = int.Parse(Console.ReadLine()); // Converting input to int directly, because later it will be used only as int
-
-
+            // add while loop until the player has money
             // 3. Create 2D array for random numbers.
 
             Random randomNumbersGenerator = new Random(); // random numbers function
             int[,] array2Dimmensional = new int[3, 3];  // columns and lines of the grid
-
             
             for (int rows = 0; rows < 3; rows++) //populating 2D Array. for every row for loop adding three columns and random numbers to it
             {
@@ -59,13 +53,24 @@ namespace The_slot_machine // Note: actual namespace depends on the project name
 
             // 5. Create if statements to see if he wants to play combination of (vertical lines, horizontal lines, only center line, two horizontal lines...)
 
-            if (playersChoseOptionToPLay == 1)
+            if (playersChoseOptionToPLay == 1) // only center line
             {
+                int first = array2Dimmensional[1, 0];
+                int second = array2Dimmensional[1, 1];
+                int third = array2Dimmensional[1, 2];
+                if(first == second && second == third)
+                {
+                    Console.WriteLine("You won!!!");
+                    // add money to players balance
+                }
+                Console.WriteLine("You lost");
+                // Take out from players balance
 
             }
 
             if (playersChoseOptionToPLay == 2)
             {
+                
 
             }
 
