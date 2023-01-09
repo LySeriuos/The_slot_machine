e95260f7ffb2949c52dfd6449a9d1b4445aeaf04
LySeriuos <drungilas.p@gmail.com>
2023-01-09 15:51:40 +0100

    added extra while statement to loop until user puts more money to account

--- The_slot_machine/Program.cs
+++ The_slot_machine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace The_slot_machine // Note: actual namespace depends on the project name.
 {
@@ -20,110 +21,84 @@ namespace The_slot_machine // Note: actual namespace depends on the project name
             // 2. Choose option for bidding (player choose amount to play $). 
 
             // add if statement to check if input is empty or null
-            Console.WriteLine("Add your amount of game money in USD $");
-            int playersGameMoney = int.Parse(Console.ReadLine()); // Converting input to int directly, because later it will be used only as int
-            while (playersGameMoney > 0)
+            int playersGameMoney;            
+
+            while (true)
             {
+                Console.WriteLine("Add your amount of game money in USD $");
+                playersGameMoney = int.Parse(Console.ReadLine()); // Converting input to int directly, because later it will be used only as int
 
-
-                Console.WriteLine("Choose how many lines you will play by typing in number of the menu");
-                Console.WriteLine("\n\t1 - Center Line. One turn 1$\n\t2 - All horizontal lines. One turn 3$\n\t3 - All vertical lines. One turn 3$\n\t4 - Diagonals. One turn 2$");
-
-                int playersChoseOptionToPLay = int.Parse(Console.ReadLine()); // Converting input to int directly, because later it will be used only as int
-                                                                              // add while loop until the player has money
-                                                                              // 3. Create 2D array for random numbers.
-
-                Random randomNumbersGenerator = new Random(); // random numbers function
-                int[,] array2Dimmensional = new int[3, 3];  // columns and lines of the grid
-
-                for (int rows = 0; rows < 3; rows++) //populating 2D Array. for every row for loop adding three columns and random numbers to it
+                while (playersGameMoney > 0)
                 {
-                    for (int columns = 0; columns < 3; columns++)
+                    Console.WriteLine("Choose how many lines you will play by typing in number of the menu");
+                    Console.WriteLine("\n\t1 - Center Line. One turn 1$\n\t2 - All horizontal lines. One turn 3$\n\t3 - All vertical lines. One turn 3$\n\t4 - Diagonals. One turn 2$");
+
+                    int playersChoseOptionToPLay = int.Parse(Console.ReadLine()); // Converting input to int directly, because later it will be used only as int
+                                                                                  // add while loop until the player has money
+                                                                                  // 3. Create 2D array for random numbers.
+
+                    Random randomNumbersGenerator = new Random(); // random numbers function
+                    int[,] array2Dimmensional = new int[3, 3];  // columns and lines of the grid
+
+                    for (int rows = 0; rows < 3; rows++) //populating 2D Array. for every row for loop adding three columns and random numbers to it
                     {
-                        array2Dimmensional[rows, columns] = randomNumbersGenerator.Next(0, 3); // attributing random numbers to every row and column
-                    }
-                }
-
-                // 4. Create and output Random Numbers machine (slot machine).
-
-                for (int rows = 0; rows < array2Dimmensional.GetLength(0); rows++) // getting the length of rows and columns from array2Dimmensional = new int[3, 3];
-                                                                                   // GetLength(0) means first number in 2 dimensional array like in here is "3"
-                                                                                   // GetLength(1) means second number in 2 dimensional array like in here is "3"
-                {
-                    for (int columns = 0; columns < array2Dimmensional.GetLength(1); columns++)
-                    {
-                        Console.Write($" {array2Dimmensional[rows, columns]}");
-                    }
-                    Console.WriteLine();
-                }
-
-
-                // 5. Create if statements to see if he wants to play combination of (vertical lines, horizontal lines, only center line, two horizontal lines...)
-                // variables for booleans
-
-                int fColFLine = array2Dimmensional[0, 0]; // getting numbers from 2D array 
-                int sColFLine = array2Dimmensional[0, 1];
-                int tColFLine = array2Dimmensional[0, 2];
-                int fColSLine = array2Dimmensional[1, 0];
-                int sColSLine = array2Dimmensional[1, 1];
-                int tColSLine = array2Dimmensional[1, 2];
-                int fColTLine = array2Dimmensional[2, 0];
-                int sColTLine = array2Dimmensional[2, 1];
-                int tColTLine = array2Dimmensional[2, 2];
-
-                // boolean for if statements with horizontal lines
-
-                bool firstLineH = fColFLine == sColFLine && sColFLine == tColFLine;
-                bool secondLineH = fColSLine == sColSLine && sColSLine == tColSLine;
-                bool thirdLineH = fColTLine == sColTLine && sColTLine == tColTLine;
-
-                // boolean for if statements with vertical lines
-
-                bool firstColumnV = fColFLine == fColSLine && fColSLine == fColTLine;
-                bool secondColumnV = sColFLine == sColSLine && sColSLine == sColTLine;
-                bool thirdColumnV = tColFLine == tColSLine && tColSLine == tColTLine;
-
-                // boolean for diagonals
-
-                bool diagonalDown = fColFLine == sColSLine && sColSLine == tColTLine;
-                bool diagonalUp = fColTLine == sColSLine && sColSLine == tColFLine;
-
-                // boolean player has money
-
-
-
-
-                if (playersGameMoney >= 1)
-                {
-                    if (playersChoseOptionToPLay == 1) // only center line
-                    {
-                        if (secondLineH)
+                        for (int columns = 0; columns < 3; columns++)
                         {
-                            Console.WriteLine("You won 2 dollars!!!");
-                            playersGameMoney += 2;
-                        }
-                        else
-                        {
-                            playersGameMoney--;
-                            Console.WriteLine("You lost!");
+                            array2Dimmensional[rows, columns] = randomNumbersGenerator.Next(0, 3); // attributing random numbers to every row and column
                         }
                     }
-                    if (playersGameMoney >= 3)
-                    {
-                        if (playersChoseOptionToPLay == 2)
-                        {
-                            if (firstLineH && secondLineH && thirdLineH)
-                            {
-                                Console.WriteLine("You won 6 dollars!!!");
-                                playersGameMoney += 6;
 
-                            }
-                            else if (firstLineH && secondLineH || firstLineH && thirdLineH || secondLineH && thirdLineH)
-                            {
-                                Console.WriteLine("You won 4 dollars !!!");
-                                playersGameMoney += 4;
-                            }
-                            else if (firstLineH | secondLineH | thirdLineH)
+                    // 4. Create and output Random Numbers machine (slot machine).
+
+                    for (int rows = 0; rows < array2Dimmensional.GetLength(0); rows++) // getting the length of rows and columns from array2Dimmensional = new int[3, 3];
+                                                                                       // GetLength(0) means first number in 2 dimensional array like in here is "3"
+                                                                                       // GetLength(1) means second number in 2 dimensional array like in here is "3"
+                    {
+                        for (int columns = 0; columns < array2Dimmensional.GetLength(1); columns++)
+                        {
+                            Console.Write($" {array2Dimmensional[rows, columns]}");
+                        }
+                        Console.WriteLine();
+                    }
+
+
+                    // 5. Create if statements to see if he wants to play combination of (vertical lines, horizontal lines, only center line, two horizontal lines...)
+                    // variables for booleans
+
+                    int fColFLine = array2Dimmensional[0, 0]; // getting numbers from 2D array 
+                    int sColFLine = array2Dimmensional[0, 1];
+                    int tColFLine = array2Dimmensional[0, 2];
+                    int fColSLine = array2Dimmensional[1, 0];
+                    int sColSLine = array2Dimmensional[1, 1];
+                    int tColSLine = array2Dimmensional[1, 2];
+                    int fColTLine = array2Dimmensional[2, 0];
+                    int sColTLine = array2Dimmensional[2, 1];
+                    int tColTLine = array2Dimmensional[2, 2];
+
+                    // boolean for if statements with horizontal lines
+
+                    bool firstLineH = fColFLine == sColFLine && sColFLine == tColFLine;
+                    bool secondLineH = fColSLine == sColSLine && sColSLine == tColSLine;
+                    bool thirdLineH = fColTLine == sColTLine && sColTLine == tColTLine;
+
+                    // boolean for if statements with vertical lines
+
+                    bool firstColumnV = fColFLine == fColSLine && fColSLine == fColTLine;
+                    bool secondColumnV = sColFLine == sColSLine && sColSLine == sColTLine;
+                    bool thirdColumnV = tColFLine == tColSLine && tColSLine == tColTLine;
+
+                    // boolean for diagonals
+
+                    bool diagonalDown = fColFLine == sColSLine && sColSLine == tColTLine;
+                    bool diagonalUp = fColTLine == sColSLine && sColSLine == tColFLine;
+
+                    // IF statements for every option
+
+                    if (playersGameMoney >= 1)
+                    {
+                        if (playersChoseOptionToPLay == 1) // only center line
+                        {
+                            if (secondLineH)
                             {
                                 Console.WriteLine("You won 2 dollars!!!");
                                 playersGameMoney += 2;
@@ -134,28 +109,77 @@ namespace The_slot_machine // Note: actual namespace depends on the project name
                                 Console.WriteLine("You lost!");
                             }
                         }
-
-                        if (playersChoseOptionToPLay == 3)
+                        if (playersGameMoney >= 3)
                         {
-                            if (firstColumnV && secondColumnV && thirdColumnV)
+                            if (playersChoseOptionToPLay == 2)
                             {
-                                Console.WriteLine("You won 6 dollars!!!");
-                                playersGameMoney += 6;
+                                if (firstLineH && secondLineH && thirdLineH)
+                                {
+                                    Console.WriteLine("You won 6 dollars!!!");
+                                    playersGameMoney += 6;
+
+                                }
+                                else if (firstLineH && secondLineH || firstLineH && thirdLineH || secondLineH && thirdLineH)
+                                {
+                                    Console.WriteLine("You won 4 dollars !!!");
+                                    playersGameMoney += 4;
+                                }
+                                else if (firstLineH | secondLineH | thirdLineH)
+                                {
+                                    Console.WriteLine("You won 2 dollars!!!");
+                                    playersGameMoney += 2;
+                                }
+                                else
+                                {
+                                    playersGameMoney--;
+                                    Console.WriteLine("You lost!");
+                                }
                             }
-                            else if (firstColumnV && secondColumnV || firstColumnV && thirdColumnV || secondColumnV && thirdColumnV)
+
+                            if (playersChoseOptionToPLay == 3)
                             {
-                                Console.WriteLine("You won 4 dollars !!!");
-                                playersGameMoney += 4;
+                                if (firstColumnV && secondColumnV && thirdColumnV)
+                                {
+                                    Console.WriteLine("You won 6 dollars!!!");
+                                    playersGameMoney += 6;
+                                }
+                                else if (firstColumnV && secondColumnV || firstColumnV && thirdColumnV || secondColumnV && thirdColumnV)
+                                {
+                                    Console.WriteLine("You won 4 dollars !!!");
+                                    playersGameMoney += 4;
+                                }
+                                else if (firstColumnV | secondColumnV | thirdColumnV)
+                                {
+                                    Console.WriteLine("You won 2 dollars!!!");
+                                    playersGameMoney += 2;
+                                }
+                                else
+                                {
+                                    playersGameMoney -= 3;
+                                    Console.WriteLine("You lost!");
+                                }
                             }
-                            else if (firstColumnV | secondColumnV | thirdColumnV)
+                        }
+
+                        if (playersGameMoney >= 2)
+                        {
+                            if (playersChoseOptionToPLay == 4)
                             {
-                                Console.WriteLine("You won 2 dollars!!!");
-                                playersGameMoney += 2;
-                            }
-                            else
-                            {
-                                playersGameMoney--;
-                                Console.WriteLine("You lost!");
+                                if (diagonalDown && diagonalUp)
+                                {
+                                    Console.WriteLine("You won 4 dollars!!!");
+                                    playersGameMoney += 4;
+                                }
+                                else if (diagonalDown || diagonalUp)
+                                {
+                                    Console.WriteLine("You won 2 dollars!!!");
+                                    playersGameMoney += 2;
+                                }
+                                else
+                                {
+                                    playersGameMoney -= 2;
+                                    Console.WriteLine("You lost!");
+                                }
                             }
                         }
                     }
@@ -163,34 +187,14 @@ namespace The_slot_machine // Note: actual namespace depends on the project name
                     {
                         Console.WriteLine("Your current balance is too low! Choose another option or add extra cash!");
                     }
-
-
-                    if (playersGameMoney >= 2)
-                    {
-                        if (playersChoseOptionToPLay == 4)
-                        {
-                            if (diagonalDown && diagonalUp)
-                            {
-                                Console.WriteLine("You won 4 dollars!!!");
-                                playersGameMoney += 4;
-                            }
-                            else if (diagonalDown || diagonalUp)
-                            {
-                                Console.WriteLine("You won 2 dollars!!!");
-                                playersGameMoney += 2;
-                            }
-                            else
-                            {
-                                playersGameMoney--;
-                                Console.WriteLine("You lost!");
-                            }
-                        }
-                    }
+                    // all the repeated code
+                    Console.WriteLine($"Your acount balance now is: {playersGameMoney} dollars");
                 }
-                // all the repeated code
-                Console.WriteLine($"Your acount balance now is: {playersGameMoney} dollars");
 
             }
+            
+
         }
+
     }
 }
